49b798ec8855cff61699bc97e405897524667017
strayosz <kamilyakupov01@gmail.com>
2024-12-11 11:37:19 +0300

    some changes

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -3,23 +3,24 @@ import module.Dog;
 import module.GuardDog;
 import module.Bird;
 import module.GuideDog;
-
-import java.security.Guard;
+import module.Pet;
 
 public class Main {
     public static void main(String[] args) {
         Cat barsik = new Cat("barsik", 10, "Black");
-        Dog sharik = new Dog("sharik", 10, "ovcharka");
+        Dog sharik = new Dog("sharik", 15, "ovcharka");
+        Pet something = new Pet("Kendra", 18);
+        something.makeSound();
         barsik.makeSound();
         sharik.makeSound();
-        GuardDog artem = new GuardDog("name", 10, "poroda", 100);
+        GuardDog bobik = new GuardDog("bobik", 9, "husky", 100);
         Bird dudu = new Bird("lulu", 2, 9);
         System.out.println(dudu.getName());
-        artem.powerFigth(6);
-        System.out.println(artem.getTrainingLevel());
-        artem.upTrainingLevel(10);
-        GuideDog alexandra = new GuideDog("name", 10, "poroda", 10, "sldjksklg");
-        alexandra.smellFight(100);
-        alexandra.commands("asjfhajshf");
+        bobik.powerFigth(6);
+        System.out.println(bobik.getTrainingLevel());
+        bobik.upTrainingLevel(10);
+        GuideDog persik = new GuideDog("persik", 10, "shpiz", 10, "middle");
+        persik.smellFight(100);
+        persik.commands("Сидеть");
     }
 }
